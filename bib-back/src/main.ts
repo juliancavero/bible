044fe2a3f00622e36b607207413eb7f322029927
juliancavero <juliancavero95@gmail.com>
2024-10-06 13:36:17 +0200

    commmit

--- bib-back/src/main.ts
+++ bib-back/src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3100'], // Permitir estos orígenes
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3100',
+      'https://bible-tawny.vercel.app',
+    ], // Permitir estos orígenes
     //origin: '*', // Permitir todos los orígenes
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     //credentials: true, // Permitir credenciales como cookies
